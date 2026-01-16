0e81dca46fe114e2f87dcd694a4e7bb9d25b3709
Mag <166319982+magali-la@users.noreply.github.com>
2026-01-15 22:49:38 -0500

    feat(routes): implement get routes to serve index and contact files

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 // import express library by requiring it
 const express = require('express');
 
+// add Node.js path module
+const path = require('path');
+
 // create an instance of an express application
 const app = express();
 
 // define the port for the server
 const port = 3001;
 
-// import paths
+// create routes and link to static files manually
 app.get("/", (req, res) => {
-    // send this when visiting the root path of the server
-    res.send('hi')
+    // send this when visiting the root path of the server and show index.html at this root path
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/contact.html"));
 })
 
 // Port Listener
